d80fb9bbc179a041c7fbf0090f34d6d7b5f4c855
Woody <devkritt@gmail.com>
2024-03-19 14:05:31 +0700

    complete socket.io

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,8 +1,12 @@
+import { useSocketContext } from '../../context/SocketContext';
 import useConversation from '../../zustand/useConversation'
 const Conversation = ({ conversation, lastIdx, emoji}) => {
 	const {selectedConversation, setSelectedConversation} = useConversation();
 
 	const isSelected = selectedConversation?._id === conversation._id;
+	const {onlineUser} = useSocketContext()
+	const isOnline = onlineUser.includes(conversation._id);
+	console.log(onlineUser)
 	return (
 		<>
 			<div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
@@ -10,7 +14,7 @@ const Conversation = ({ conversation, lastIdx, emoji}) => {
 			`}
 			onClick={()=>setSelectedConversation(conversation)}
 			>
-				<div className='avatar online'>
+				<div className={`avatar ${isOnline?"online":""}`}>
 					<div className='w-12 rounded-full'>
 						<img
 							src={conversation.profilePic}
