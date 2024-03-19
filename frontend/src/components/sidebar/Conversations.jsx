4489519f8bfc689de9ef2b80744cd1dddd0ef8f0
Woody <devkritt@gmail.com>
2024-03-19 13:27:58 +0700

    complete messaging feature

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Conversation from "./Conversation";
-
+import useGetConversation from "../../hooks/useGetConversation";
+import { getRandomEmoji } from '../../utils/emoji'
 export default function Conversations() {
+  const {loading, conversation} = useGetConversation();
+  console.log(conversation)
   return (
     <div className="py-2 flex flex-col overflow-auto">
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
+
+        {conversation.map((c, idx) => (
+          <Conversation 
+            key={c._id} 
+            conversation={c}
+            emoji={getRandomEmoji()}
+            lastIdx={idx === conversation.length - 1}
+          />
+        ))}
+
+        {loading ? <span className="loading loading-spinner mx-auto"></span> : null}
     </div>
   )
 }
--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import {useState} from "react";
 import GenderCheckbox from "./GenderCheckbox";
+import { Link } from "react-router-dom";
+import useSignup from "../../hooks/useSignup";
 
 export default function Signup() {
+  const [input, setInput] = useState({
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    gender: ''
+  })
+
+  const {loading, signup} = useSignup();
+
+  const handleCheckboxChange = (gender) => {
+    setInput({...input, gender});
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    await signup(input)
+  }
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -9,7 +29,8 @@ export default function Signup() {
           Login
           <span className="text-blue-500 "> ChatApp</span>
         </h1>
-        <form>
+
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Full Name</span>
@@ -18,6 +39,8 @@ export default function Signup() {
               type="text"
               placeholder="John Doe"
               className="w-full input input-bordered h-10"
+              value={input.fullName}
+              onChange={(e)=>setInput({...input, fullName: e.target.value})}
             />
           </div>
 
@@ -29,6 +52,8 @@ export default function Signup() {
               type="text"
               placeholder="johndoe"
               className="w-full input input-bordered h-10"
+              value={input.username}
+              onChange={(e)=>setInput({...input, username: e.target.value})}
             />
           </div>
 
@@ -40,6 +65,8 @@ export default function Signup() {
               type="text"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={input.password}
+              onChange={(e)=>setInput({...input, password: e.target.value})}
             />
           </div>
 
@@ -51,20 +78,25 @@ export default function Signup() {
               type="text"
               placeholder="Enter Confirm Password"
               className="w-full input input-bordered h-10"
+              value={input.confirmPassword}
+              onChange={(e)=>setInput({...input, confirmPassword: e.target.value})}
             />
           </div>
 
-          <GenderCheckbox />
+          <GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={input.gender} />
 
-          <a
-            href="#"
+          <Link
+            to="/login"
             className="text-sm hoverF:underline hover:text-blue-600 mt-2 inline-block"
           >
-            {"Don't"} have an account?
-          </a>
+            Already have an account?
+          </Link>
 
           <div>
-          <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+          <button className="btn btn-block btn-sm mt-2 border border-slate-700"
+          disabled={loading}>
+             {loading ? <span className="loading loading-spinner"></span> : "Sign Up"}
+          </button>
           </div>
         </form>
       </div>
